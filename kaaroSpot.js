d5551d0f82bae109ca1ebeb29bf7cf98b13730d9
karx <kartik.arora1214@gmail.com>
2020-01-17 22:30:38 +0530

    init for Team Spot-ME

---
+++ kaaroSpot.js
@@ -0,0 +1,9 @@
+
+
+// const net = await posenet.load();
+const net = await posenet.load({
+    architecture: 'ResNet50',
+    outputStride: 32,
+    inputResolution: { width: 257, height: 200 },
+    quantBytes: 2
+  });
